d20738a784337d4fca2d63f6927955ecd9173b1f
tiffanyfl <tiffanyf.levacher@gmail.com>
2018-03-06 14:47:03 +0100

    Bookings.js

--- routes/bookings.js
+++ routes/bookings.js
@@ -1,20 +1,48 @@
-var _ = require('lodash');
-var express = require('express');
-var request = require('request-promise');
-var router = express.Router();
+'use strict';
+const {expect} = require('chai');
+const supertest = require('supertest');
+const request = supertest('http://localhost:3000');
 
-router.post('/', async (req, res, next) => {
-	const {body} = req;
-  const authentification = req.get('authentification');
-  if(!authentification) {
-    return res.status(401).send({message: 'il faut envoyer le token'});
-  }
+describe('Test des endpoints /bookings', function() {
+	describe('création de reservation, POST /reservations', function() {
+		it('si la requete ne content pas les identifiants de l\'utilisateur, on reçoit un 401', function() {
+      return request
+				.post('/bookings')
+				.send({id_user: '123', id_flight: 'AF-444'})
+				.expect(401);
+    });
 
-  if(_.)
-  if(!body.id_user || !body.id_flight){
-    return res.status(400).send({message: "Il manque des données"})
-  }
-	return res.status(201).send({reference: "123"});
+		it('avec un user identifié, mais pas de body, on reçoit un 400', function() {
+      return request
+				.post('/bookings')
+				.set('authentification', 'MY_TOKEN')
+				.send()
+				.expect(400);
+    });
+
+    it('si la requete a toutes les infos, on reçoit un 201 et la reference', function() {
+      return request
+				.post('/bookings')
+				.set('authentification', 'MY_TOKEN')
+				.send({id_user: '123', id_flight: 'AF-123'})
+				.expect(201)
+				.then(response => {
+					const body = response.body;
+					expect(body).to.have.property('reference');
+				});
+    });
+	});
+
+  describe('consultation de reservation, GET /reservations/{id}', function() {
+    it('si la reservation n\'existe pas, on reçoit un 404');
+
+    it('si la requete ne content pas les identifiants de l\'utilisateur, on reçoit un 401');
+
+    it('si la reservation existe et appartient à l\'utilisateur, on reçoit un 200');
+	});
+
+  describe('annulation de reservation, DELETE /reservations/{id}', function() {
+		it('si la reservation n\'existe pas, on reçoit un 404');
+    //etc etc etc
+	});
 });
-
-module.exports = router;
--- test/booking.js
+++ test/booking.js
@@ -3,37 +3,46 @@ const {expect} = require('chai');
 const supertest = require('supertest');
 const request = supertest('http://localhost:3000');
 
-describe('booking API', function() {
-  describe('When booking for a flight', function() {
-    it('without flight id should return a 400 error', function(){
+describe('Test des endpoints /bookings', function() {
+	describe('création de reservation, POST /reservations', function() {
+		it('si la requete ne content pas les identifiants de l\'utilisateur, on reçoit un 401', function() {
       return request
-        .post('/bookings')
-        .send({user_id: '1'})
-        .expect(400, err => {
-          console.log(err);
-        });
+				.post('/bookings')
+				.send({id_user: '123', id_flight: 'AF-444'})
+				.expect(401);
     });
 
-    it('without authentification should return a 401 error', function(){
+		it('avec un user identifié, mais pas de body, on reçoit un 400', function() {
       return request
-        .post('/bookings')
-        .set({user_id: '123', id_flight: "AF-444"})
-        .expect(401, err => {
-          console.log(err);
-        });
+				.post('/bookings')
+				.set('authentification', 'MY_TOKEN')
+				.send()
+				.expect(400);
     });
 
-    it('si la requete a toutes les infos, on reçoit un 201 et la reference', function(){
+    it('si la requete a toutes les infos, on reçoit un 201 et la reference', function() {
       return request
-        .post('/bookings')
-        .set({'authentification', 'MY_TOKEN')
-        .send({id_user: '123', id_flight: 'AF-123'})
-        .expect(201)
-        .then(responde => {
-          
-        });
+				.post('/bookings')
+				.set('authentification', 'MY_TOKEN')
+				.send({id_user: '123', id_flight: 'AF-123'})
+				.expect(201)
+				.then(response => {
+					const body = response.body;
+					expect(body).to.have.property('reference');
+				});
     });
+	});
 
-    it('with all data should return a 201');
-  });
+  describe('consultation de reservation, GET /reservations/{id}', function() {
+    it('si la reservation n\'existe pas, on reçoit un 404');
+
+    it('si la requete ne content pas les identifiants de l\'utilisateur, on reçoit un 401');
+
+    it('si la reservation existe et appartient à l\'utilisateur, on reçoit un 200');
+	});
+
+  describe('annulation de reservation, DELETE /reservations/{id}', function() {
+		it('si la reservation n\'existe pas, on reçoit un 404');
+    //etc etc etc
+	});
 });
